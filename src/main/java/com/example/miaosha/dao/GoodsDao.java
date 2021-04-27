e47eba8da77a47937b947ac0e6b8dccb07ab03e4
soulbiubiubiu <1172807506@qq.com>
2021-04-27 20:45:56 +0800

    卖超、负库存问题

--- src/main/java/com/example/miaosha/dao/GoodsDao.java
+++ src/main/java/com/example/miaosha/dao/GoodsDao.java
@@ -16,6 +16,8 @@ public interface GoodsDao {
     List<GoodsVo> listGoodsVo();
     @Select("select g.*,mg.stock_count, mg.start_date, mg.end_date,mg.miaosha_price from miaosha_goods mg left join goods g on mg.goods_id = g.id where g.id=#{goodsId}")
     GoodsVo getGoodsVoById(@Param("goodsId") long goodsId);
-    @Update("update miaosha_goods set stock_count=stock_count-1 where goods_id=#{goodsId}")
+    //and stock_count>0 解决库存为负
+    //卖超解决 建立miaosha_order表的唯一索引（goods_id, order_id）  如果同一个用户触发了两次，第二次会回滚。
+    @Update("update miaosha_goods set stock_count=stock_count-1 where goods_id=#{goodsId} and stock_count>0")
     int reduceStock(MiaoshaGoods g);
 }
